e9f0e8ac58bbab32eeb52c22adc67d611a98ee1e
Daniel N <2color@users.noreply.github.com>
2025-09-01 21:13:36 +0200

    chore: upgrade eslint

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import js from '@eslint/js'
+import typescript from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+import react from 'eslint-plugin-react'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+import globals from 'globals'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+        ...globals.jasmine,
+        ...globals.es2015
+      }
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      react: react,
+      prettier: prettier
+    },
+    settings: {
+      react: {
+        pragma: 'React',
+        version: 'detect'
+      }
+    },
+    rules: {
+      ...prettierConfig.rules,
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true
+        }
+      ],
+      'react/prop-types': 'off',
+      'react/display-name': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/naming-convention': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off'
+    }
+  }
+]
